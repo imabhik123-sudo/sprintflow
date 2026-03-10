a02c840148fa08f77026fc6053299222f097cb80
imabhik123-sudo <imabhik.123@gmail.com>
2026-03-10 08:40:40 +0530

    Add firebaseConfig.js with Firebase settings

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+firebaseConfig.js:
+```javascript name=firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAj5Fr64gC4zkgWEbQPqMWNbJDYpygnwII",
+  authDomain: "studio-717156718-d0bf7.firebaseapp.com",
+  projectId: "studio-717156718-d0bf7",
+  storageBucket: "studio-717156718-d0bf7.firebasestorage.app",
+  messagingSenderId: "153956380800",
+  appId: "1:153956380800:web:72923ed30c9aadbbf13665"
+};
+
+module.exports = firebaseConfig;
